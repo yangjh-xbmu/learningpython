c1da68acb39dee5c636815f6d6697a9e455d1c1c
yangjh <yangjh@yeah.net>
2018-09-18 14:23:50 +0800

    重新生成

--- _book/gitbook.sh
+++ _book/gitbook.sh
@@ -1,10 +1,6 @@
 git checkout master
+gitbook build
 git add .
 git commit -m $1
 git push -u origin master
-git checkout pages
-cp -r _book/* .
-git add .
-git commit -m $1
-git push -u origin pages
-git checkout master
+open https://yangjh.gitee.io/learningpython/
--- gitbook.sh
+++ gitbook.sh
@@ -1,4 +1,5 @@
 git checkout master
+gitbook build
 git add .
 git commit -m $1
 git push -u origin master
