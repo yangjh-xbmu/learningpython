88c975410ea3d504848b04030e63b87ae8a7bc1c
yangjh <yangjh@yeah.net>
2019-12-21 13:35:56 +0800

    增加提示信息

--- push.sh
+++ push.sh
@@ -2,9 +2,7 @@ git add .
 git commit -m $1
 git push -u origin master
 git push -u github master
-echo '/n'
-echo '文档信息推送完毕。开始推送静态文件仓库'
-echo '/n'
+echo '\n 文档信息推送完毕，开始推送静态文件仓库。 \n'
 cd ../bookdown-pub/learningpython
 git add .
 git commit -m $1
