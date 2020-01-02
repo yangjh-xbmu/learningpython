4f4d61696367de73cdb0d8eaa6466a0b93f2a49b
yangjh <yangjh@yeah.net>
2020-01-02 10:31:34 +0800

    增加pipenv内容

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git add .
 git commit -m $1
 git push -u origin master
 git push -u github master
-echo '\n 文档信息推送完毕，开始推送静态文件仓库。 \n'
+echo '\n文档信息推送完毕，开始推送静态文件仓库。 \n'
 cd ../bookdown-pub/learningpython
 git add .
 git commit -m $1
